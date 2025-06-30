bff8f22ab40ed78c4f4cd51e63adb37bdfde3f69
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-06-30 07:02:04 +0200

    Create assets/js/footer.js
    Dodat footer.js

---
+++ assets/assets/js/footer.js
@@ -0,0 +1,11 @@
+// Dodaje noopener noreferrer automatski svim eksternim linkovima
+document.querySelectorAll('.footer a[target="_blank"]').forEach(link => {
+    if (!link.getAttribute('rel')) {
+        link.setAttribute('rel', 'noopener noreferrer');
+    }
+});
+
+// Dodaje ARIA atribute
+const ctaButtons = document.querySelectorAll('.action-buttons a');
+ctaButtons[0].setAttribute('aria-label', 'Podrži Melektron projekat');
+ctaButtons[1].setAttribute('aria-label', 'Zakažite sastanak sa Milanom');
