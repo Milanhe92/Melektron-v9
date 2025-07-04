2c8139dea9bfa9ff27824bf25781266ba3efbe79
Milan He <179525809+Milanhe92@users.noreply.github.com>
2025-07-05 01:32:05 +0200

    Create src/app/layout.jsx
    Dodajem validan layout.jsx u src/app

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Melektron',
+  description: 'Kvantni hram večnog kapitala',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sr">
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif', backgroundColor: '#000', color: '#0f0' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
